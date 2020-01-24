bfc61299ca7d9eceb987375d6556673b6bffbe76
JanTheBeast <44895646+JanTheBeast@users.noreply.github.com>
2020-01-24 10:11:50 +0100

    cleaned up TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTouch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTouch.java
@@ -8,61 +8,146 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "TankDriveTouch", group = "")
 public class TankDriveTouch extends OpMode{
+
+    //declaring motor variables
     private DcMotor motorLeft;
     private DcMotor motorRight;
     private DcMotor intakeRight;
     private DcMotor intakeLeft;
+
+    //declaring servo variables
     private Servo capStone1;
     private Servo capStone2;
     private Servo claw1;
     private Servo claw2;
+
+    //declaring sensor variables
     private DigitalChannel sensorTouch;
 
-    int intakeState;
-
-    int xbutton;
-    //boolean intakeMotorOut;
-   // boolean intakeMotorIn;
-    //boolean intakeSwitchOut;
-   // boolean intakeSwitchIn;
-
-    double drivedirectionspeed;
+    //declaring variables
+    private int intakeState;
+    private double drivedirectionspeed;
 
     @Override
     public void init() {
+        //initializing motor names
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
         intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
         intakeRight = hardwareMap.dcMotor.get("intakeRight");
+
+        //initializing servo names
         capStone1 = hardwareMap.servo.get("capStone1");
         capStone2 = hardwareMap.servo.get("capStone2");
         claw1 = hardwareMap.servo.get("claw1");
         claw2 = hardwareMap.servo.get("claw2");
 
-        intakeState = 0;
-        xbutton = 0;
-        //intakeMotorIn = false;
-        //intakeMotorOut = false;
-        //intakeSwitchIn = false;
-        //intakeSwitchOut = false;
+        //initializing sensor names
         sensorTouch = hardwareMap.digitalChannel.get("sensorTouch");
 
+        //initializing variables
+        intakeState = 0;
         drivedirectionspeed = 0.75;
     }
 
     @Override
-    public void loop() { DriveChecks(); }
+    public void loop() {
+        DriveChecks();
+        IntakeChecks();
+        ClawChecks();
+    }
 
-    void DriveChecks() {
+    //Drivecheck function to keep loop() clean
+    private void DriveChecks() {
+
+        //the variables for the motor speeds
         double right = -gamepad1.right_stick_y * drivedirectionspeed;
         double left = gamepad1.left_stick_y * drivedirectionspeed;
-        double intake = 1;
 
 //--------------------------------------------------------------------------
 
+        //setting the motor speeds
         motorLeft.setPower(left);
         motorRight.setPower(right);
 
+//--------------------------------------------------------------------------
+
+        //clamping the dds and making it go up and down with a button
+        if (gamepad1.right_trigger > 0.5 && drivedirectionspeed < 1) {
+            drivedirectionspeed += 0.005f;
+        } else if (gamepad1.left_trigger > 0.5 && drivedirectionspeed > 0.2) {
+            drivedirectionspeed -= 0.005f;
+        } else if (gamepad1.right_bumper){
+            drivedirectionspeed = 1f;
+        } else if (gamepad1.left_bumper){
+            drivedirectionspeed = 0.33f;
+        }
+
+        telemetry.addData("DriveDirectionSpeed", drivedirectionspeed);
+        telemetry.update();
+    }
+
+    //intakechecks function to keep loop() clean
+    private void IntakeChecks() {
+
+        //standard speed for motors
+        double intake = 1;
+
+        //state maching which switches between the intakestates
+        switch(intakeState){
+            case 0:
+                //intake is off
+                intakeLeft.setPower(0);
+                intakeRight.setPower(0);
+
+                if(gamepad2.y){
+                    intakeState = 10;
+                }
+
+                break;
+
+            case 10:
+                //intake takes in block
+                intakeLeft.setPower(-intake);
+                intakeRight.setPower(intake);
+
+                //when block is picked up is goes on
+                if(!sensorTouch.getState()){
+                    intakeState = 20;
+                }else if(gamepad2.x){
+                    //when the it is on with no block you can turn it off with x
+                    intakeState = 0;
+                }
+                break;
+
+            case 20:
+                //intake is off
+                intakeLeft.setPower(0);
+                intakeRight.setPower(0);
+
+                if(gamepad2.x){
+                    intakeState = 30;
+                }
+
+                break;
+
+            case 30:
+                //intake spits out block
+                if(gamepad2.x){
+                    intakeLeft.setPower(intake);
+                    intakeRight.setPower(-intake);
+                }else if(!gamepad2.x){
+                    intakeState = 0;
+                }
+
+                break;
+        }
+    }
+
+    //--------------------------------------------------------------------------
+
+    //Clawcheck function to keep loop() clean
+    private void ClawChecks() {
 
         if(gamepad2.dpad_up){
             capStone1.setPosition(0);
@@ -80,64 +165,5 @@ public class TankDriveTouch extends OpMode{
             claw2.setPosition(0.40);
         }
 
-//--------------------------------------------------------------------------
-
-        switch(intakeState){
-            case 0:
-                intakeLeft.setPower(0);
-                intakeRight.setPower(0);
-
-                if(gamepad2.y){
-                    intakeState = 10;
-                }
-
-                break;
-
-            case 10:
-                intakeLeft.setPower(-intake);
-                intakeRight.setPower(intake);
-
-                if(sensorTouch.getState() == false){
-                    intakeState = 20;
-                }else if(gamepad2.x){
-                    intakeState = 0;
-                }
-                break;
-
-            case 20:
-                intakeLeft.setPower(0);
-                intakeRight.setPower(0);
-
-                if(gamepad2.x){
-                    intakeState = 30;
-                }
-
-                break;
-
-            case 30:
-                if(gamepad2.x){
-                    intakeLeft.setPower(intake);
-                    intakeRight.setPower(-intake);
-                }else if(!gamepad2.x){
-                    intakeState = 0;
-                }
-
-                break;
-        }
-
-//--------------------------------------------------------------------------
-
-        if (gamepad1.right_trigger > 0.5 && drivedirectionspeed < 1) {
-            drivedirectionspeed += 0.005f;
-        } else if (gamepad1.left_trigger > 0.5 && drivedirectionspeed > 0.2) {
-            drivedirectionspeed -= 0.005f;
-        } else if (gamepad1.right_bumper){
-            drivedirectionspeed = 1f;
-        } else if (gamepad1.left_bumper){
-            drivedirectionspeed = 0.33f;
-        }
-
-        telemetry.addData("DriveDirectionSpeed", drivedirectionspeed);
-        telemetry.update();
     }
 }
