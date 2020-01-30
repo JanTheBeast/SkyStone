6df8a340972a6d42ec3d1793614e8a5122705ae4
JanTheBeast <janvanbaast@live.nl>
2020-01-30 19:23:30 +0100

    drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Drivetrain.java
@@ -1,12 +1,21 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+import org.firstinspires.ftc.teamcode.Math.Constants;
+import org.firstinspires.ftc.teamcode.Math.Mathfunc;
 
 public class Drivetrain {
     DcMotor frontLeft;
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
+    BNO055IMU imu;
 
     public Drivetrain (DcMotor frontLeft, DcMotor frontRight, DcMotor backLeft, DcMotor backRight){
         frontRight.setDirection(DcMotor.Direction.REVERSE);
@@ -24,11 +33,41 @@ public class Drivetrain {
 
     }
 
+    public void DriveTrainMecanum(){
+        backLeft = null;
+        frontLeft = null;
+        frontRight = null;
+        backRight = null;
+        imu = null;
+    }
+
     public void setPower(double flpower, double frpower, double blpower, double brpower){
         frontLeft.setPower(flpower);
         frontRight.setPower(frpower);
         backLeft.setPower(blpower);
         backRight.setPower(brpower);
+    }
+
+    public void turnToAngle(double angle, double speed, double precision){
+            double lastError;
+            long lastTime;
+        while(!Mathfunc.range(3, imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle,angle), precision){
+
+            double error = angle - imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES). thirdAngle;
+            long currTime = System.currentTimeMillis();
+            double dE = error - lastError;
+            long dT = currTime - lastTime;
+            double correction = (error * Constants.Kp)+((dE/dT)*Constants.Kd) + Constants.Ci;
+            correction = Mathfunc.clamp(-1,1,correction);
+            setPower(correction, -correction, correction, -correction);
+            lastError = error;
+            lastTime = currTime;
+        }
+        setPower(0,0,0,0);
+    }
+
+    public void ForwardCorrection(int time, double speed){
+        double startAngle =
     }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Constants.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Math;
+
+public class Constants {
+
+    //PID constants
+    public static final double Kp = 0.1;
+    public static final double Kd = 0.2;
+    public static final double Ci = 0.15;
+    public static final double X = 0.1;
+
+
+}
